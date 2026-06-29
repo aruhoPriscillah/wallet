b8321785bb232d6f6662de415262f7eb151a31ec
aruhoPriscillah <priscaaruho@gmail.com>
2026-06-29 10:27:38 +0300

    Add recipient

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Wallet, Transaction, TransferRequest
+from .models import Wallet, Transaction, TransferRequest, Recipient
+
 
 
 @admin.register(Wallet)
